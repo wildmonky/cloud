bc4bc2373220d3f677fd6ccf444a482b23014f16
wildmonky <1525920492@qq.com>
2022-06-05 17:22:29 +0800

    基础项目（接口，实体），删除.idea文件夹

---
+++ gateway/src/main/java/org/lizhao/cloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,12 @@
+package org.lizhao.cloud.gateway.config;
+
+/**
+ * Description TODO
+ *
+ * @author lizhao
+ * @version 1.0.0
+ * @date 2022-06-05 13:30
+ * @since jdk-1.8.0
+ */
+public class GatewayConfig {
+}
---
+++ gateway/src/main/java/org/lizhao/cloud/gateway/controller/RouteController.java
@@ -0,0 +1,12 @@
+package org.lizhao.cloud.gateway.controller;
+
+/**
+ * Description TODO
+ *
+ * @author lizhao
+ * @version 1.0.0
+ * @date 2022-06-05 16:19
+ * @since jdk-1.8.0
+ */
+public class RouteController {
+}
