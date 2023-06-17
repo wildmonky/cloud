c11a4399a6d647b195086d7f167eebd11f13b4ae
wildmonky <1525920492@qq.com>
2023-06-17 17:53:19 +0800

    结构调整

--- gateway/src/main/java/org/lizhao/cloud/gateway/configurer/nosql/mongo/MongoConfigurer.java
+++ gateway/src/main/java/org/lizhao/cloud/gateway/configurer/nosql/mongo/MongoConfigurer.java
@@ -1,8 +1,5 @@
 package org.lizhao.cloud.gateway.configurer.nosql.mongo;
 
-import jakarta.annotation.Resource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
