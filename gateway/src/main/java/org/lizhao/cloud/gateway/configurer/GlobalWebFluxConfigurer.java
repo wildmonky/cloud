e4e1f63e11762dde2f07986aaddaa470fb8424bf
wildmonky <1525920492@qq.com>
2023-07-02 23:34:36 +0800

    RouteDefinition、FilterDefinition类型转化 构造函数优化

--- gateway/src/main/java/org/lizhao/cloud/gateway/configurer/GlobalWebFluxConfigurer.java
+++ gateway/src/main/java/org/lizhao/cloud/gateway/configurer/GlobalWebFluxConfigurer.java
@@ -1,10 +1,23 @@
 package org.lizhao.cloud.gateway.configurer;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.lizhao.cloud.web.react.json.decoder.PredicateDefinitionDecoder;
 import org.lizhao.cloud.web.react.json.decoder.RouteDefinitionDecoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.AbstractJackson2Decoder;
 import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.io.IOException;
 
 /**
  * Description webflux全局配置
@@ -37,8 +50,25 @@ public class GlobalWebFluxConfigurer implements WebFluxConfigurer {
     @Override
     public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
         //设置自定义request body 类型转换
-        configurer.customCodecs().register(new RouteDefinitionDecoder());
+//        configurer.customCodecs().register(new RouteDefinitionDecoder());
         configurer.defaultCodecs().enableLoggingRequestDetails(true);
+        configurer.defaultCodecs().configureDefaultCodec(codec -> {
+            if (codec instanceof AbstractJackson2Decoder abstractJackson2Decoder) {
+                SimpleModule simpleModule = new SimpleModule();
+                simpleModule.addDeserializer(PredicateDefinition.class, new JsonDeserializer<PredicateDefinition>() {
+                    @Override
+                    public PredicateDefinition deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+                        JsonToken jsonToken = p.nextToken();
+                        while(jsonToken != null) {
+                            jsonToken = p.nextToken();
+                            System.out.println(jsonToken);
+                        }
+                        return null;
+                    }
+                });
+                abstractJackson2Decoder.getObjectMapper().registerModule(simpleModule);
+            }
+        });
     }
 
 }
